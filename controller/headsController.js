af416e86e8686f4f3293f4b993e56d4c070f5408
omar <anza.omar12@gmail.com>
2021-04-18 13:21:01 +0500

    Changes by Omar

--- controller/headsController.js
+++ controller/headsController.js
@@ -1,7 +1,7 @@
 const db = require("../models");
 
 exports.getHead = (req, res) => {
-	db.Head.find().sort({ hid: 1 })
+	db.Head.find().sort({ _id: 1 })
     .then((head) =>{
         res.status(200).json(head);
     });
